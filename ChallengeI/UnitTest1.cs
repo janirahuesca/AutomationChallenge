88f909535f1051e595377f8db36e3efe7b652b62
janirahuesca-inspladelestany <janirahuesca@inspladelestany.cat>
2024-07-12 09:50:42 +0100

    Add project files.

---
+++ ChallengeI/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ChallengeI
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
