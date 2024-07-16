e04053728b4b1a9cfb5f0aea36af22f02e7881a9
janirahuesca-inspladelestany <janirahuesca@inspladelestany.cat>
2024-07-16 19:46:28 +0100

    feat(challengeI): Removed comments

--- ChallengeI/StepDefinitions/ChallengeIStepDefinitions.cs
+++ ChallengeI/StepDefinitions/ChallengeIStepDefinitions.cs
@@ -9,45 +9,45 @@ namespace ChallengeI.StepDefinitions
     [Binding] 
     public sealed class ChallengeIStepDefinitions
     {
-        private IWebDriver _driver; // Instancia del WebDriver para interactuar con el navegador
-        private LinksPage _linksPage; // Instancia de la página de objetos para interactuar con los elementos de la página        
+        private IWebDriver _driver;
+        private LinksPage _linksPage;    
         private BasePage _currentPage = null!;
 
-        [BeforeScenario] // Método ejecutado antes de cada escenario de prueba
+        [BeforeScenario]
         public void Setup()
         {
-            _driver = new ChromeDriver(); // Inicializa ChromeDriver para Chrome
-            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); // Establece un tiempo de espera implícito
-            _linksPage = new LinksPage(_driver); // Inicializa la página de objetos pasándole el WebDriver
+            _driver = new ChromeDriver();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _linksPage = new LinksPage(_driver); 
         }
 
-        [AfterScenario] // Método ejecutado después de cada escenario de prueba
+        [AfterScenario] 
         public void TearDown()
         {
             if (_driver != null)
             {
-                _driver.Quit(); // Cierra el navegador
-                _driver = null; // Libera recursos
+                _driver.Quit(); 
+                _driver = null; 
             }
         }
 
-        [Given(@"a user navigates to the website ""([^""]*)""")] // Navegar a una URL específica
+        [Given(@"a user navigates to the website ""([^""]*)""")] 
         public void GivenAUserNavigatesToTheWebsite(string url)
         {
-            _driver.Navigate().GoToUrl(url); // Navegamos a la URL especificada
+            _driver.Navigate().GoToUrl(url); 
         }
 
-        [Then(@"the first selected button should be ""([^""]*)""")] // Verificar el nombre del primer botón seleccionado
+        [Then(@"the first selected button should be ""([^""]*)""")] 
         public void ThenTheFirstSelectedButtonShouldBe(string expectedButtonName)
         {
-            string firstButtonName = _linksPage.GetFirstButtonText(); // Obtiene el nombre del primer botón
+            string firstButtonName = _linksPage.GetFirstButtonText(); 
             Assert.AreEqual(expectedButtonName, firstButtonName, $"Expected '{expectedButtonName}' button, but found '{firstButtonName}'."); // Compara con el nombre esperado y afirmamos
         }
 
-        [When(@"the user clicks on the ""([^""]*)"" button")] // Hacer clic en un botón específico
+        [When(@"the user clicks on the ""([^""]*)"" button")] 
         public void WhenTheUserClicksOnTheButton(string buttonName)
         {
-            _linksPage.ClickButton(buttonName); // Hacemos clic en el botón especificado
+            _linksPage.ClickButton(buttonName); 
 
             switch (buttonName.ToLowerInvariant()) 
             {
@@ -70,27 +70,27 @@ namespace ChallengeI.StepDefinitions
             }
         }
 
-        [Then(@"the user should see the ""([^""]*)"" page")] // Verificar que se vea una página específica
+        [Then(@"the user should see the ""([^""]*)"" page")] 
         public void ThenTheUserShouldSeeThePage(string expectedPageContent)
         {
-            string actualPageContent = _linksPage.GetPageContent(); // Obtenemos el contenido de la página
+            string actualPageContent = _linksPage.GetPageContent(); 
             Assert.IsTrue(actualPageContent.Contains(expectedPageContent, StringComparison.OrdinalIgnoreCase), $"Expected to find '{expectedPageContent}' in page content."); // Verificamos que el contenido esperado esté presente en la página
         }
 
-        [When(@"the user click on the ""GO BACK"" button")] // Hacer clic en el botón "GO BACK"
+        [When(@"the user click on the ""GO BACK"" button")] 
         public void WhenTheUserClickOnTheGoBackButton()
         {
             if (_currentPage is IReturnable page) 
             {
-                page.ClickGoBack(); // Hacemos clic en el botón "GO BACK"
+                page.ClickGoBack(); 
             }
         }
 
-        [Then(@"the user should be back on the home page")] // Verificar que se esté de vuelta en la página de inicio
+        [Then(@"the user should be back on the home page")] 
         public void ThenTheUserShouldBeBackOnTheHomePage()
         {
             string actualUrl = _driver.Url;
-            Assert.AreEqual("https://qaplayground.dev/apps/links/", actualUrl, "Expected to be back on the home page."); // Verificamos que el nombre del botón sea "HOME"
+            Assert.AreEqual("https://qaplayground.dev/apps/links/", actualUrl, "Expected to be back on the home page."); 
         }
     }
 }
