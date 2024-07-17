b0e973db937a29ddbc41fcf9707aae540f393892
janirahuesca-inspladelestany <janirahuesca@inspladelestany.cat>
2024-07-17 17:59:24 +0100

    feat(challengeII): implementation of the step definitions. Test is passing with success

---
+++ ChallengeII/PageObjects/IFrame1Page.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+
+namespace ChallengeII.PageObjects
+{
+    internal class IFrame1Page : BasePage
+    {
+        public static string Id => "frame1";
+
+        public IFrame1Page(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private IWebElement InnerIframe => _driver.FindElement(By.CssSelector("#frame2"));
+
+        public void SwitchToInnerIframe() 
+        {
+            SwitchToIframe(InnerIframe.GetAttribute("id"));
+        }
+    }
+}
---
+++ ChallengeII/PageObjects/IFrame2Page.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+
+namespace ChallengeII.PageObjects
+{
+    internal class IFrame2Page : BasePage
+    {
+        private const string ButtonSelector = ".btn.btn-green-outline"; // Selector del botón dentro del iframe
+        private const string MessageSelector = "#msg"; // Selector del mensaje que aparece después de hacer clic en el botón
+
+        public IFrame2Page(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void ClickButton()
+        {
+            var button = _driver.FindElement(By.CssSelector(ButtonSelector)); // Encuentra el botón
+            button.Click(); // Haz clic en el botón
+        }
+
+        public string GetMessage()
+        {
+            var message = _driver.FindElement(By.CssSelector(MessageSelector)).Text; // Encuentra el mensaje y obtiene el texto
+            return message; // Devuelve el texto del mensaje
+        }
+    }
+}
